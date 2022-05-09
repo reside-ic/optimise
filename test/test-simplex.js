f3fc0ba81f0e2167f3d28ab8a32703dbaabf6852
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-05-09 14:13:00 +0100

    Test initialisation

--- test/test-simplex.js
+++ test/test-simplex.js
@@ -2,6 +2,7 @@
 
 var expect = require("chai").expect;
 var simplex = require("../lib/simplex.js");
+var control = require("../lib/control.js");
 
 describe('optimise simple problem', () => {
     // multivariate quadratic
@@ -25,3 +26,24 @@ describe('optimise simple problem', () => {
         expect(res.fx).to.be.at.most(1e-5);
     });
 });
+
+
+describe('configure initial simplex', () => {
+    var target = (x) => x.map(x => x * x).reduce((a, b) => a + b, 0);
+
+    it("starting points away from zero are scaled", () => {
+        var ctl = control.simplexControl({deltaNonZero: 0.5});
+        var obj = new simplex.Simplex(target, [2, 4], ctl);
+        expect(obj._simplex[0].x).to.eql([2, 4]);
+        expect(obj._simplex[1].x).to.eql([3, 4]);
+        expect(obj._simplex[2].x).to.eql([2, 6]);
+    });
+
+    it("starting points at zero are scaled", () => {
+        var ctl = control.simplexControl({deltaNonZero: 0.5, deltaZero: 0.1});
+        var obj = new simplex.Simplex(target, [0, 2], ctl);
+        expect(obj._simplex[0].x).to.eql([0, 2]);
+        expect(obj._simplex[1].x).to.eql([0.1, 2]);
+        expect(obj._simplex[2].x).to.eql([0, 3]);
+    });
+})
